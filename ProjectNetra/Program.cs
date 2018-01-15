fad62da5f8d553f842532d427a721893c99b7558
sangamo <sangamo@DESKTOP-N6B23RD>
2018-01-14 16:00:40 -0800

    adding the gmail api

---
+++ ProjectNetra/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ProjectNetra.MailClasses;
+
+namespace ProjectNetra
+{
+    class Check
+    {
+        public static void Main(string[] args)
+        {
+            GMail g = new GMail();
+            g.Test();
+        }
+    }
+}
